4f8c5722111b4b7e584bf543e2c26ac32532c71a
Sungatullin Bulat <sungatullinbulat05@gmail.com>
2021-05-23 11:00:08 +0300

    init

---
+++ benchmark/benchmark.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <chrono>
+
+// подключаем вашу структуру данных
+#include "algorithm.hpp"
+
+
+using namespace std;
+using namespace itis;
+
+// абсолютный путь до набора данных и папки проекта
+static constexpr auto kDatasetPath = string_view{PROJECT_DATASET_DIR};
+static constexpr auto kProjectPath = string_view{PROJECT_SOURCE_DIR};
+
+int main() {
+    string path = "C:\\Users\\4aspe\\Desktop\\working\\semester-work-bellman-ford\\dataset\\";
+    const string test_data_name = "data";
+    path += test_data_name;
+
+    long int count_top[] = {10, 50, 100, 500, 1000};
+    const int SETS_NUMBER = 5;
+    const int FILE_NUMBER = 10;
+    for (int set_n = 0; set_n < SETS_NUMBER; set_n++) {
+      cout << "---------SET NUMBER " << set_n + 1<< "---------" << endl;
+
+      long double middle_time = 0;
+      for (int file_n = 1; file_n <= FILE_NUMBER; file_n++) {
+        string file_path = path + "\\" + to_string(count_top[set_n]) + "\\" + to_string(file_n) + ".txt";
+        ifstream file(file_path);
+
+        int n = 0;
+        int** matrix = create_graph(file, n);
+
+        bool has_negative_cycle = false;
+        const auto time_point_before = chrono::steady_clock::now();  // запоминаем время начала операции
+        int* array_top = algorithm(matrix, n, has_negative_cycle);
+        const auto time_point_after = chrono::steady_clock::now();  // запоминаем время конца операции
+        // измеряем разницу по времени
+        const auto time_diff = time_point_after - time_point_before;
+        // переводим время в наносекунды
+        const long double time_elapsed_ns = chrono::duration_cast<chrono::nanoseconds>(time_diff).count();
+
+        cout << "Time taken: " << time_elapsed_ns / 1000000 << '\n';
+        //cout << (has_negative_cycle ? "Graph has negative cycle" : "Graph hasn't negative cycle") << endl;
+        middle_time += time_elapsed_ns / 1000000;
+
+        delete_matrix(matrix, n);
+        delete[] array_top;
+        file.close();
+      }
+        middle_time /= 10;
+        cout << "Middle time = " << middle_time << endl;
+        cout << "--------------------------------" << endl;
+    }
+}
---
+++ dataset/generate_txt_dataset.py
@@ -0,0 +1,36 @@
+from os import mkdir, curdir
+from os.path import abspath
+from random import randint
+
+path = abspath(curdir) + "\\data"
+
+count_top = [10, 50, 100, 500, 1_000]  # квадраты чисел - кол-во элементов
+# count_top = [100, 2500, 10000, 250000, 1000000]
+
+COUNT_GRAPHS = 10
+LEFT = -300
+RIGHT = 300
+COUNT_FOLDERS = 5
+
+if __name__ == "__main__":
+    top_n = 0
+
+    for inside_folder in count_top:
+        mkdir(path + "\\" + str(inside_folder))
+
+        for graph in range(1, COUNT_GRAPHS + 1):
+            file = open(path + "\\" + str(inside_folder) + "\\" + str(graph) + ".txt", "w", newline="")
+
+            with file:
+                top_number = count_top[top_n]
+                file.write(str(top_number) + "\n")
+
+                for i in range(top_number):
+                    for g in range(top_number):
+                        file.write(str(randint(LEFT, RIGHT)) + " ")
+                    file.write("\n")
+
+        top_n += 1
+        print(inside_folder, " generated")
+
+    print("DONE!")
